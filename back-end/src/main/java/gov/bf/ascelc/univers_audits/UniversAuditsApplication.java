96333d00e869f03d0cff7d7f010a3c19d3317fec
Issouf SOULI <issouf.souli@asce-lc.bf>
2026-03-09 16:53:22 +0000

    premier commit

---
+++ back-end/src/main/java/gov/bf/ascelc/univers_audits/UniversAuditsApplication.java
@@ -0,0 +1,13 @@
+package gov.bf.ascelc.univers_audits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversAuditsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversAuditsApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/gov/bf/ascelc/univers_audits/UniversAuditsApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.bf.ascelc.univers_audits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversAuditsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
