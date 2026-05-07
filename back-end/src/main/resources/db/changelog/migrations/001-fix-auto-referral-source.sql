4ecbc3fd7166e7be8f831f05dd50a009661ec432
abdoulsiakourdrabo <118267733+abdoulsiakourdrabo@users.noreply.github.com>
2026-05-07 17:57:56 +0000

    Regle probleme typage db

---
+++ back-end/src/main/resources/db/changelog/migrations/001-fix-auto-referral-source.sql
@@ -0,0 +1,5 @@
+﻿-- liquibase formatted sql
+
+-- changeset dev:001-fix-auto-referral-source
+ALTER TABLE dossier ALTER COLUMN auto_referral_source TYPE VARCHAR(30);
+-- rollback ALTER TABLE dossier ALTER COLUMN auto_referral_source TYPE SMALLINT;
